024888b150dcc3a1d85f5156e71836e6b6c80daf
Juan Manuel Vilches <juanmanuelvilches99@gmail.com>
2025-04-30 11:40:17 -0300

    Ejercicio 7 terminado

---
+++ JS/ejercicio7.js
@@ -0,0 +1,20 @@
+// Pide al usuario una frase y cuenta cuántas palabras tiene.
+function frase () {
+   const palabra = prompt("Ingrese una frase")
+   return palabra
+}
+do{
+    frase()
+
+}
+while(!frase)
+
+
+
+
+
+
+// console.log(frase)
+console.log(Boolean(frase))
+// console.log(arrayDeFrase)
+// console.log(arrayDeFrase.length)
