facbd57027a5f9ee5dd5366adf556cccab1ed1a5
Juan Manuel Vilches <juanmanuelvilches99@gmail.com>
2025-05-01 16:36:56 -0300

    Ejercicio 11 avanzado

--- JS/ejercicio11.js
+++ JS/ejercicio11.js
@@ -2,3 +2,28 @@
 
 const fechaNacimiento = prompt("Ingrese su fecha de nacimiento en formato YYYY-MM-DD")
 
+const hoy = new Date(fechaNacimiento)
+const actual = new Date()
+const resta = actual - hoy
+console.log (resta)
+console.log (hoy)
+console.log(hoy.getFullYear())
+console.log(hoy.getMonth())
+// const numero = Number(hoy)
+// console.log (numero)
+
+// const input = prompt("Ingresa tu fecha de nacimiento (formato YYYY-MM-DD):");
+
+// const nacimiento = new Date(input);
+// const hoy = new Date();
+
+// // Verifica si la fecha es válida
+// if (isNaN(nacimiento)) {
+//     alert("Fecha inválida. Asegúrate de usar el formato correcto: YYYY-MM-DD");
+// } else {
+//     const milisegundosPorDia = 1000 * 60 * 60 * 24;
+//     const diferencia = hoy - nacimiento;
+//     const edadEnDias = Math.floor(diferencia / milisegundosPorDia);
+
+//     alert(`Tenés aproximadamente ${edadEnDias} días de edad.`);
+// }
